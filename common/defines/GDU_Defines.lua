710bb2d9588203d5761476df896eae69daa0b636
Zaini99 <mr.zain.issa@gmail.com>
2023-07-17 15:37:31 +0100

    Smol Naval Fix

--- common/defines/GDU_Defines.lua
+++ common/defines/GDU_Defines.lua
@@ -639,9 +639,9 @@ NDefines.NNavy.GUN_HIT_PROFILES = { -- hit profiles for guns, if target ih profi
 	   }					
 NDefines.NNavy.DEPTH_CHARGES_HIT_PROFILE = 300.0    -- hit profile for depth charges	
 NDefines.NNavy.BASE_GUN_COOLDOWNS = { -- number of hours for a gun to be ready after shooting
-		3.3,	-- big guns
-		2.7,	-- torpedos
-		1.3,	-- small guns
+		3.2,	-- big guns
+		2.5,	-- torpedos
+		1.2,	-- small guns
 	}
 			
 NDefines.NNavy.COMBAT_BASE_HIT_CHANCE = 1.0                                    -- base chance for hit
