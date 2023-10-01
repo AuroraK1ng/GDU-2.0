195b256fcdabc89394be264f1a5b7e29cd9769c1
Barss1ck <mrtemus@gmail.com>
2023-10-01 17:27:41 +0300

    .

---
+++ common/defines/cm_graphics.lua
@@ -0,0 +1,152 @@
+
+NDefines_Graphics.NGraphics.MAX_MESHES_LOADED_PER_FRAME = 5
+
+--NDefines_Graphics.NGraphics.DRAW_DETAILED_CUTOFF = 400
+
+NDefines_Graphics.NMapMode.MAP_MODE_TERRAIN_TRANSPARENCY = 0.8
+NDefines_Graphics.NMapMode.RADAR_ROTATION_SPEED = 0.0
+
+
+
+
+NDefines_Graphics.NMapMode.AIR_RANGE_INDICATOR_ROTATION_SPEED = 0.000									-- How quickly is that indicator rotating
+
+NDefines_Graphics.NAirGfx.AIRPLANES_SMOOTH_INTERPOLATION_MOVE = 0.02 	-- How smooth is the movement interpolation.
+NDefines_Graphics.NAirGfx.AIRPLANES_SMOOTH_INTERPOLATION_TURN = 0.02 	-- How smooth is the turning interpolation.
+NDefines_Graphics.NAirGfx.MAX_MISSILE_BOMBING_SCENARIOS = 0
+NDefines_Graphics.NAirGfx.MAX_BOMBING_SCENARIOS = 0
+NDefines_Graphics.NAirGfx.MAX_PATROL_SCENARIOS = 0
+NDefines_Graphics.NAirGfx.MAX_DOGFIGHTS_SCENARIOS = 0
+NDefines_Graphics.NAirGfx.MAX_TRANSPORT_SCENARIOS = 0
+NDefines_Graphics.NAirGfx.AIRPLANES_ANIMATION_GLOBAL_SPEED_PER_GAMESPEED = {0.3, 0.3, 0.3, 0.3, 0.3, 0.3}
+
+
+NDefines_Graphics.NInterface.GRIDBOX_ELEMENTS_INTERPOLATION_SPEED = 0.2 -- A value used to determine how fast the elements within the gridbox are interpolating while drag'n dropping.
+
+NDefines_Graphics.NGraphics.TREE_FADE_NEAR = 150.0
+NDefines_Graphics.NGraphics.TREE_FADE_FAR = 200.0
+
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {250, 350, 500} -- At what distance VPs are hidden
+NDefines_Graphics.NGraphics.RADAR_ICON_DISTANCE_CUTOFF = 100 -- At what distance the radars are hidden
+NDefines_Graphics.NGraphics.RESOURCE_MAP_ICON_TEXT_CUTOFF = 450  -- At what camera distance the resource name/amount text disappears.
+NDefines_Graphics.NGraphics.PROVINCE_ANIM_TEXT_DISTANCE_CUTOFF = 200
+NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 400	-- At what camera distance capital icons disappears
+NDefines_Graphics.NGraphics.UNITS_DISTANCE_CUTOFF = 100
+NDefines_Graphics.NGraphics.SHIPS_DISTANCE_CUTOFF = 200
+NDefines_Graphics.NGraphics.UNIT_ARROW_DISTANCE_CUTOFF = 500
+NDefines_Graphics.NGraphics.UNITS_ICONS_DISTANCE_CUTOFF = 700
+NDefines_Graphics.NGraphics.NAVAL_COMBAT_DISTANCE_CUTOFF = 1000
+NDefines_Graphics.NGraphics.ADJACENCY_RULE_DISTANCE_CUTOFF = 1300
+NDefines_Graphics.NGraphics.LAND_COMBAT_DISTANCE_CUTOFF = 800
+NDefines_Graphics.NGraphics.SUPPLY_ICON_DISTANCE_CUTOFF = 1500
+NDefines_Graphics.NGraphics.PROV_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 300
+NDefines_Graphics.NGraphics.STATE_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.DECISION_MAP_ICON_DISTANCE_CUTOFF = 100
+
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_REFRESH_FREQ = 0.2 -- how frequent is gradient borders repainting (optimization for high-speed gameplay)
+NDefines_Graphics.NGraphics.MAPICON_GROUP_PASSES = 5 -- how many mapicons get processed per frame for grouping. more = quicker response, fewer = betteperformancer 
+
+NDefines_Graphics.NGraphics.DRAW_REFRACTIONS_CUTOFF = 0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_FIELD_COUNTRY_REFRESH = 40 -- When country changes it's size by X provinces, then it refresh it's thickness and rebuilds all provinces
+--NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 1.0					-- Remove map objects at this distance
+
+NDefines_Graphics.NGraphics.WEATHER_DISTANCE_CUTOFF = 50 -- At what distance weather effects are hidden
+
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {300, 500, 1500}
+
+--NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_MAX_SIZE = 0
+NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 100				--group moving and still units
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 325.0		--group into states
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 400		--group units into air regions
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 220					--size limit for air region grouping
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_HUGE = 100							--size limit for state grouping
+NDefines_Graphics.NGraphics.MAPICON_GROUP_STRATEGIC_SIZE = 1000
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 300
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 0
+NDefines_Graphics.NGraphics.BLOOM_WIDTH = 0
+NDefines_Graphics.NGraphics.BLOOM_SCALE = 0
+NDefines_Graphics.NGraphics.BRIGHT_THRESHOLD = 0
+NDefines_Graphics.NGraphics.EMISSIVE_BLOOM_STRENGTH = 0
+NDefines_Graphics.NGraphics.DAY_NIGHT_FEATHER = 0.024
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_CUTOFF = 0
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_FADE_LENGTH = 0
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 0
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0
+
+NDefines_Graphics.NAirGfx.AIRPLANES_ANIMATION_GLOBAL_SPEED_PER_GAMESPEED = {0.3, 0.3, 0.3, 0.3, 0.3, 0.3}
+NDefines_Graphics.NGraphics.COMMANDGROUP_PRESET_COLORS_HSV = {
+	0.0/360.0, 1.0, 1.0,	--red
+	10.0/360.0, 1.0, 1.0,	--orange
+	60.0/360.0, 1.0, 1.0,	--yellow
+	120.0/360.0, 0.75, 1.0,	--green
+	180.0/360.0, 1.0, 1.0,	--turq
+	235.0/360.0, 1.0, 1.0,	--blue
+	260.0/360.0, 1.0, 1.0,	--dark purple
+	300.0/360.0, 1.0, 1.0,	--light purple
+	330.0/360.0, 0, 1.0		--white
+}
+
+NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 20
+
+NDefines_Graphics.NGraphics.AIRBASE_ICON_DISTANCE_CUTOFF = 900
+NDefines_Graphics.NGraphics.NAVALBASE_ICON_DISTANCE_CUTOFF = 900
+
+--NDefines_Graphics.NGraphics.UNIT_ARROW_DISTANCE_CUTOFF = 1000
+--NDefines_Graphics.NGraphics.UNITS_ICONS_DISTANCE_CUTOFF = 900
+--NDefines_Graphics.NGraphics.NAVAL_COMBAT_DISTANCE_CUTOFF = 1500
+--NDefines_Graphics.NGraphics.ADJACENCY_RULE_DISTANCE_CUTOFF = 1700
+--NDefines_Graphics.NGraphics.LAND_COMBAT_DISTANCE_CUTOFF = 1500
+--NDefines_Graphics.NGraphics.SUPPLY_ICON_DISTANCE_CUTOFF = 2500
+--NDefines_Graphics.NGraphics.PROV_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 400
+--NDefines_Graphics.NGraphics.STATE_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 800
+
+--NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_BAD = {0.65, 0, 0, 1}
+--NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_GOOD = {0, 0.65, 0, 1}
+--NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_AVERAGE = {0.65, 0.65, 0, 1}
+--NDefines_Graphics.NGraphics.STRATEGIC_AIR_COLOR_NEUTRAL = {130.0/255, 130.0/255, 130.0/255, 1}
+
+--NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 250.0
+--NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 250 --250.0
+--NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 250 --250.0
+	
+NDefines_Graphics.NGraphics.RESISTANCE_COLOR_GOOD = {0.0, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.RESISTANCE_COLOR_AVERAGE = {0.65, 0.65, 0, 1}
+NDefines_Graphics.NGraphics.RESISTANCE_COLOR_BAD = {0.65, 0, 0, 1}
+	
+NDefines_Graphics.NGraphics.STRATEGIC_NAVY_COLOR_MISSION = {0.65, 0.65, 0.0, 1}
+NDefines_Graphics.NGraphics.STRATEGIC_NAVY_COLOR_NEUTRAL = {130.0/255, 130.0/255, 130.0/255, 1}
+
+NDefines_Graphics.NGraphics.ROOT_FRONT_OFFSET = 2
+--NDefines_Graphics.NAirGfx.AIRPLANES_ANIMATION_GLOBAL_SPEED_PER_GAMESPEED = {0.3, 0.3, 0.3, 0.3, 0.3, 0.3}
+
+--NDefines_Graphics.NGraphics.VIRTUAL_BATTLEPLANS_COLOR = { 1.0, 0.0, 0.0, 1 }
+
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_R = 0.5	--state connected to province
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_G = 0.5
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_B = 0.5
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_A = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_SUPPLY_AREA_R = 0.6
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_SUPPLY_AREA_G = 0.2
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_SUPPLY_AREA_B = 0.6
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_SUPPLY_AREA_A = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_ADJACENCY_RULE_AREA_R = 0.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_ADJACENCY_RULE_AREA_G = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_ADJACENCY_RULE_AREA_B = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_ADJACENCY_RULE_AREA_A = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_BUILDING_AREA_R = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_BUILDING_AREA_G = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_BUILDING_AREA_B = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_BUILDING_AREA_A = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_R = 1.0 --selected province or region
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_G = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_B = 0.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_A = 1.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_CUSTOM_HIGHLIGHT_R = 0.0
+--NDefines_Graphics.NGraphics.BORDER_COLOR_CUSTOM_HIGHLIGHT_G = 0.61
+--NDefines_Graphics.NGraphics.BORDER_COLOR_CUSTOM_HIGHLIGHT_B = 0.75
+--NDefines_Graphics.NGraphics.BORDER_COLOR_CUSTOM_HIGHLIGHT_A = 1.0
+--NDefines_Graphics.NGraphics.WATER_MAP_MODE_COLOR_R = 1.0
+--NDefines_Graphics.NGraphics.WATER_MAP_MODE_COLOR_G = 0.0
+--NDefines_Graphics.NGraphics.WATER_MAP_MODE_COLOR_B = 0.0
+
+--NDefines_Graphics.NGraphics.BORDER_WIDTH = 2
---
+++ resources/HistoryStates.cs
@@ -0,0 +1,137 @@
+﻿using System.Text.RegularExpressions;
+
+namespace history {
+    public class State {
+        public string file;
+        public int ownerLine;
+        public string owner;
+        public Dictionary<string,int> cores = new Dictionary<string, int>();
+    }
+    class HistoryStates {
+        static void Main(string[] args) {
+            string[] stateFiles = Directory.GetFiles("../history/states","*.txt");
+            string observer = "SWE";
+            string wastelandTag = "WST";
+            // The shit that isn't going to be wasteland
+            string[] tags = new String[43] {observer,"ALB","AST","AUS","BEL","BRA","BUL","CAN","CHI","CZE","DEN","ENG","EST","FIN","FRA","GER","GRE","HOL","HUN","IRE","IRQ","ITA","JAP","LAT","LIT","LUX","MAN","MEX","NOR","NZL","POL","RAJ","ROM","SAF","SIA","SOV","SPR","USA","VEN","VIC","YUG","ETH","PER"};
+            // TagBeingReplaced TagToReplaceWith
+            Dictionary<string,string> replaceTags = new Dictionary<string, string>(){
+                {"INS","HOL"},
+                {"PHI","USA"},
+                {"GXC","CHI"},
+                {"YUN","CHI"},
+                {"SHX","CHI"},
+                {"PRC","CHI"},
+                {"SIK","CHI"},
+                {"XSM","CHI"},
+                {"AFA","ETH"},
+                {"MEN","MAN"},
+                {"MAL","ENG"}
+            };
+            // Shit to not core to new owner but core to observer
+            string[] dontCore = {"MAL","PHI","INS"};
+            List<State> states = new List<State>();
+            // Tags to make wasteland in no asia, put original owner tag in vanilla, might have to do shit like indochina manually boohoo
+            string[] noAsia = {"USA","AST","NZL","MEX","RAJ","MAN","SIA","JAP","INS","MAL","PHI","CHI","MEN","YUN","GXC","SIK","SHX","XSM","PRC"};
+
+            /////////////////////////////////////////////////////////
+
+            for (int i = 0; i < stateFiles.Length; i++) {
+                states.Add(getState(stateFiles[i]));
+                processState(states[i], observer, wastelandTag, tags, replaceTags, dontCore, noAsia);
+            }
+            Console.WriteLine("DONE");
+
+            Console.ReadKey();
+        }
+
+        public static State getState(string file) {
+            State state = new State();
+            state.file = file;
+            string[] fileLines = File.ReadAllLines(file);
+            Dictionary<string,int> owners = new Dictionary<string, int>();
+            for (int i = 0; i < fileLines.Length; i++) {
+                if (Regex.IsMatch(fileLines[i],@".*owner =.*")) {
+                    owners.Add(fileLines[i].Substring(fileLines[i].Length-3),i);
+                }
+                if (Regex.IsMatch(fileLines[i],@".*add_core_of =.*") && !state.cores.ContainsKey(fileLines[i].Substring(fileLines[i].Length-3))) {
+                    
+                    state.cores.Add(fileLines[i].Substring(fileLines[i].Length-3),i);
+                }
+            }
+
+            if(owners.Count !=0 ) { // You want to add the first owner, incase there is some history that adds a second owner
+                state.owner = owners.First().Key;
+                state.ownerLine = owners.First().Value;
+            }
+
+            return state;
+        }
+
+        public static void processState(State state, string observer, string wastelandTag, string[] tags, Dictionary<string,string> replaceTags, string[] dontCore, string[] noAsia) {
+            string[] fileLines = File.ReadAllLines(state.file);
+            Console.WriteLine(state.owner);
+            for (int i = 0; i < fileLines.Length; i++) {
+                if(i == state.ownerLine) {
+                    if(noAsia.Contains(state.owner)) {
+                        for (int x = 0; x < fileLines.Length; x++) {
+                            if(Regex.IsMatch(fileLines[x],@".*history.*")) {
+                                if(Regex.IsMatch(fileLines[x],@".*{.*")) {
+                                    fileLines[x] = "history={ 1936.1.1.13 = { owner = WST add_core_of = WST }";
+                                } else {
+                                    fileLines[x+1] = "{ 1936.1.1.13 = { owner = WST add_core_of = WST }";
+                                }
+                            }
+                        }
+                    }
+
+                    // Replace tags, if owner is in replaceTags then replace owner with correct one
+                    if(replaceTags.ContainsKey(state.owner)) {
+                        fileLines[i] = fileLines[i].Replace(state.owner,replaceTags[state.owner]);
+                        if(dontCore.Contains(state.owner)) {
+                            fileLines[state.cores[state.owner]] = fileLines[state.cores[state.owner]].Replace(state.owner,observer);
+                        } else {
+                            fileLines[state.cores[state.owner]] = fileLines[state.cores[state.owner]].Replace(state.owner,replaceTags[state.owner]);
+                        }
+                    }
+
+                    // Wasteland, if owner isn't in tags replace with wastelandTag
+                    if(!tags.Contains(state.owner) && !replaceTags.ContainsKey(state.owner) && state.owner != "RAJ") { // RAJ Doesn't have cores on Indian land in history files nor is there any effect that adds them but they still magically get them
+                        fileLines[i] = "        owner = " + wastelandTag;
+                         for (int x = 0; x < fileLines.Length; x++) {
+                            if(Regex.IsMatch(fileLines[x],@".*manpower =.*")) { // Set manpower to 1
+                                fileLines[x] = "    manpower = 1";
+                            }
+                            if(Regex.IsMatch(fileLines[x],@".*victory_points =.*")) { // Remove victory points
+                                int y = x;
+                                while (!Regex.IsMatch(fileLines[y],@".*}.*"))
+                                {
+                                    fileLines[y] = "";
+                                    y++;
+                                }
+                                fileLines[y] = ""; // Or closing bracket wont get removed
+                            }
+                        }
+                    } else {
+                        if(state.cores.ContainsKey(state.owner)) { // Colony cores, if owner is in tags and it's owner has a core then make them all owner cores
+                            
+                            foreach (var kvp in state.cores) {
+                                fileLines[kvp.Value] = fileLines[kvp.Value].Replace(kvp.Key,state.owner);
+                            }
+                        } else { // Unnecesary cores, if owner is in tags and it's owner doesn't have a core then make then all observer cores
+                            foreach (var kvp in state.cores) {
+                                if(!tags.Contains(kvp.Key)) { // Keep colony cores for tags, eg Nefoundland and Labrador should stay Canadian core
+                                    fileLines[kvp.Value] = fileLines[kvp.Value].Replace(kvp.Key,observer);
+                                }
+                            }
+                        }
+                    }
+
+                    
+                }
+            }
+            File.WriteAllText(state.file, string.Join("\r\n", fileLines) + "\r\n");
+        }
+      
+    }
+}
